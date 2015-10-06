737cd764df05daac9fdcf161256b9cf07a93ade8
melvinzhang <melvinzhang@gmail.com>
2015-10-06 09:15:51 +0800

    add other double operations to sample C code

--- examples/fp-ops.c
+++ examples/fp-ops.c
@@ -3,5 +3,8 @@
 int main() {
     int a = printf("%s\n", "Hello");
     printf("%f\n", a + 1.1);
+    printf("%f\n", a - 1.1);
+    printf("%f\n", a * 1.1);
+    printf("%f\n", a / 1.1);
     return 0;
 }
